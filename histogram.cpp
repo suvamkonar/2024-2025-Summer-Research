778d98b044e10bfd6e1fe035da6268f92aa9033a
suvamkonar <120138834+suvamkonar@users.noreply.github.com>
2024-05-23 17:43:32 -0400

    Update histogram.cpp

--- histogram.cpp
+++ histogram.cpp
@@ -1,19 +1,122 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
 using namespace std;
+
+// FILE OPENING
+ifstream fin;
+ofstream fout;
+string InputFileName;
+string OutputFileName;
+void setIOOpen(string name) {
+
+	InputFileName = name + ".txt";
+	OutputFileName = name + ".txt";
+
+	fout.open(OutputFileName, ios::out | ios::trunc);
+
+	if (!fout) {
+		fout << "Unable to open file \n";
+		exit(1); // terminate with error
+	}
+
+	fin.open(InputFileName, ios::in);
+
+	if (!fin) {
+		fout << "Unable to open file \n";
+		exit(1); // terminate with error
+	}
+}
+//FILE CLOSING
+void setIOClose()
+{ 
+	fin.close();
+	fout.close();
+}
+
+
+int numInterval;
+int amountData;
+
+vector<pair<int,int>> intervals;
+vector<int> dataHolder;
 
 void read()
 {
+    int a; //reads in the number of intervals
+	fin >> a;
+	numInterval = a;
+
+	int b; //reads in the number of data values
+	fin >> b;
+	amountData = b;
     
+    for(int i = 0; i < numInterval; i++) { // reads in the intervals in form of pairs
+		int start;
+		int end;
+		fin >> start >> end;
+		intervals.push_back( make_pair(start, end) );
+	}
+
+	for(int i = 0; i < amountData; i++) { //reads in the raw data itself
+		int value;
+		fin >> value;
+		dataHolder.push_back(value);
+	}
+
+	sort(dataHolder.begin(), dataHolder.end()); //sort the data
 }
 
-void build()
+void build() 
 {
-    
+
+	vector<int> intervalCounts;
+
+    for (int i = 0; i < intervals.size(); i++) { //goes through the intervals
+		int start = intervals[i].first; // start of interval
+		int end = intervals[i].second; //end of interval
+
+		int frequency = 0; //how many data values are in current interval
+
+		for (int value: dataHolder) {
+			if(start <= value <= end) {
+				frequency++;
+			}
+		}
+
+		intervalCounts.push_back(frequency); // enters frequency for the interval
+	}
+
+	// MAKING HISTOGRAM
+	cout << "DATA HISTOGRAM";
+    for (int i = 0; i < intervals.size(); i++) { //goes through the intervals
+		int start = intervals[i].first; // start of interval
+		int end = intervals[i].second; //end of interval
+		cout << start << "-" << end;
+
+		cout.width(2); 
+        cout << " | "; 
+		
+		int freq = intervalCounts[i];
+		for(int j = 0; j < freq; j++) {
+			cout << " x "; 
+		}
+
+		cout << endl;
+
+	}
+    cout << "---------------------------------------" << endl; 
+    cout << "    "; 
+  
+  
 }
 
 int main()
 {
+    setIOOpen("data");
     read();
     build();
+    setIOClose();
     return 0;
 }
